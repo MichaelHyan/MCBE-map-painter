0404ef357d5917e136fa9278971eebec90bbcf18
MichaelHyan <129747814+MichaelHyan@users.noreply.github.com>
2025-09-19 00:05:26 +0800

    Add files via upload

--- map_painter/map_painter_plus.py
+++ map_painter/map_painter_plus.py
@@ -12,7 +12,6 @@ def match(image_path,color):
         print(f"读取图片失败：{e}")
         return
     width, height = img.size
-    print(f"\n图片尺寸：{width}x{height} 像素")
     result = []
     for y in range(height):
         for x in range(width):
@@ -40,12 +39,42 @@ def match(image_path,color):
                     closest_idx = color_idx
             result.append((x, y, closest_idx))
     return result
-def convert(x,y,l,block):
+def convert_a(x,y,l,block):
     return f'setblock ~{x} ~-1 ~{y} {block[l]}'
-def main():
+
+def convert_b(x,y,z,block):
+    return f'setblock ~{x} ~{z-1} ~{y} {block}'
+
+def tower(x,y,l):
+    t1 =[]
+    t2 = [[[0,0]]]
+    for i in range(l):
+        t1 = []
+        for j in range(i+1):
+            t1.append([j,i-j+1])
+            t1.append([-j,-i+j-1])
+            t1.append([i-j+1,-j])
+            t1.append([-i+j-1,j])
+        t2.append(t1)
+    for i in t2:
+        for j in i:
+            j[0] += x
+            j[1] += y
+    return t2
+
+def position(x,y,x0):
+    return x + y * x0
+
+def main_a():
     global m_list,c_list,weight
     print('将图片拖至此处')
     path = input()
+    try:
+        size = Image.open(path).convert("RGB").size
+    except Exception as e:
+        pass
+    x0,y0 = size
+    print(f'图片尺寸：{x0}*{y0}')
     print('选取材料类型，序号使用空格分开')
     for i in range(len(material)):
         print(f'{i} {name[i]} {material[i]}')
@@ -70,7 +99,66 @@ def main():
         for j in i:
             mt_list.append(j)
     for x, y, l in result:
-        r.append(convert(x,y,l,mt_list))
+        r.append(convert_a(x,y,l,mt_list))
+    j = 0
+    for i in range(0,len(r),9000):
+        with open(f'.\\functions\\new{j}.mcfunction', 'w') as f:
+            f.write('\n'.join(r[i:i+9000]))
+        j += 1
+    print(f'生成完毕  共{j}个文件')
+    print('是否生成预览图，y/n')
+    if input() == 'y':
+        pix = []
+        for x,y,l in result:
+            pix.append((x,y,(c_list[l][0],c_list[l][1],c_list[l][2])))
+        width = max(x for x, y, rgb in pix) + 1
+        height = max(y for x, y, rgb in pix) + 1
+        image = Image.new("RGB", (width, height))
+        for x, y, rgb in pix:
+            image.putpixel((x, y), rgb)
+        image.save("output_image.png")
+
+def main_b():
+    global m_list,c_list,weight
+    print('提示：运行前先在脚底垫2格')
+    print('将图片拖至此处')
+    path = input()
+    print('是否使用视觉增强算法，y/n')
+    if input() == 'y':
+        weight = True
+    else:
+        weight = False
+    with open(f'.\\material\\map_fallable_b.txt','r') as f:
+        b = f.read().split('\n')
+        m_list.append(b)
+    with open(f'.\\material\\map_fallable_c.txt','r') as f:
+        c = f.read().split('\n')
+    for j in c:
+        c_list.append(list(map(int,j.split(','))))
+    result = match(path,c_list)
+    try:
+        size = Image.open(path).convert("RGB").size
+    except Exception as e:
+        pass
+    x0,y0 = size
+    print(f'图片尺寸：{x0}*{y0}')
+    print('输入起始点位置')
+    x1,y1 = map(int,input().split())
+    l = max(x0,y0)
+    t = tower(x1,y1,l*2)
+    r = [f'setblock ~{x1} ~-2 ~{y1} torch',f'setblock ~{x1} ~-1 ~{y1} sand']
+    for i in range(len(t)-1):#level
+        for j in range(len(t[i])):
+            if t[i][j][0] >= -1*x1 and t[i][j][1] >= -1*y1 and t[i][j][0] < (x0-x1) and t[i][j][1] < (y0-y1):
+                block = m_list[0][result[position(t[i][j][0]+x1,t[i][j][1]+y1,x0)][2]]
+                r.append(convert_b(t[i][j][0],t[i][j][1],i,block))
+        for j in range(len(t[i+1])):
+            if t[i+1][j][0] >= -1*x1 and t[i+1][j][1] >= -1*y1 and t[i+1][j][0] < (x0-x1) and t[i+1][j][1] < (y0-y1):
+                r.append(convert_b(t[i+1][j][0],t[i+1][j][1],i,'torch'))
+    for j in range(len(t[-1])):#final
+        if t[-1][j][0] >= -1*x1 and t[-1][j][1] >= -1*y1 and t[-1][j][0] < (x0-x1) and t[-1][j][1] < (y0-y1):
+            block = m_list[0][result[position(t[i][j][0]+x1,t[i][j][1]+y1,x0)][2]]
+            r.append(convert_b(t[i][j][0],t[i][j][1],i,block))
     j = 0
     for i in range(0,len(r),9000):
         with open(f'.\\functions\\new{j}.mcfunction', 'w') as f:
@@ -90,4 +178,10 @@ def main():
         image.save("output_image.png")
 
 if __name__ == "__main__":
-    main()
+    print('1 地图画模式')
+    print('2 沙画模式')
+    a = input()
+    if a == '1':
+        main_a()
+    elif a == '2':
+        main_b()
